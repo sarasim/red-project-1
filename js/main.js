26bcbba7849321644303c2d3901733a36c5f2edb
Sara Sim <sarasim@Saras-MacBook-Air.local>
2016-03-18 22:15:04 -0700

    Initiate smooth scrolling.

--- js/main.js
+++ js/main.js
@@ -23,11 +23,29 @@ $(function(){
       event.preventDefault();
 
       if ( $( '#your-email'). val().length !== 0 ) {
-            alert('Thanks for signing up!');        
+            alert('Thanks for signing up!');
 
       } else {
             alert('Please enter a valid email address!');
       }
 
     });
+});
+
+/**
+*Smoothscroll*/
+
+$(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
 });
